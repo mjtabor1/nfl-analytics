82fabe42449d4ca4d9473705de930b8d8f851c85
Michael Tabor <mjtabor23@gmail.com>
2025-09-16 17:53:37 -0600

    server/setup: initial migrations and server config

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from .config import Config
-from .extensions import db, migrate, jwt, cors
+from config import Config
+from extensions import db, migrate, jwt, cors
 # from .routes.auth import auth_bp
 # from .routes.dashboards import dashboards_bp
 # from .routes.players import players_bp
@@ -13,13 +13,16 @@ def create_app():
     cors(app, origins=[app.config["FRONTEND_ORIGIN"]], supports_credentials=True)
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
     jwt.init_app(app)
 
     # app.register_blueprint(auth_bp, url_prefix="/api/auth")
     # app.register_blueprint(dashboards_bp, url_prefix="/api/dashboards")
     # app.register_blueprint(players_bp, url_prefix="/api/players")
     # app.register_blueprint(ai_bp, url_prefix="/api/ai")
+    import controllers.nfl.models
+    from flask_migrate import Migrate
+    Migrate(app, db)
 
     @app.get("/api/healthz")
     def health():
