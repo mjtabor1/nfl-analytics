f153eef90b5ec7fc82025c59b05eece059a57247
Michael Tabor <mjtabor23@gmail.com>
2025-09-23 19:06:43 -0600

    default routing and components with sleepper api integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <nav>
-        <Link to="dashboard">Dashboard</Link> |{" "}
-        <Link to="profile">Profile</Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
-}
-
-function Dashboard() {
-  return <div>Dashboard Page</div>;
-}
-
-function Profile() {
-  return <div>Profile Page</div>;
-}
+import HomePage from './pages/Home'
 
 function App() {
 
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="home" element={<Home />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
+        <Route path="/login" element={<div>Here is our login page</div>} />
+        <Route path="/signup" />
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/dashboards/:id" />
       </Routes>
     </BrowserRouter>
   )
